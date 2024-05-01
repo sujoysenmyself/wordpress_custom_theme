26dfd17db23b28480f9deb7f3399a914377cd21a
sujoysenmyself <sujoysenjitu1994@gmail.com>
2024-05-01 23:03:58 +0530

    blog loop is displayed in the index page

--- index.php
+++ index.php
@@ -1,330 +1,38 @@
-<?php 
-  get_header();
+<?php
+    get_header();
 ?>
 
 
-    <!-- slider section -->
-
-    <!-- end slider section -->
-  </div>
-
-  <!-- do section -->
-
-  <section class="do_section layout_padding">
+    <!-- Body Contend is started here -->
     <div class="container">
-      <div class="heading_container">
-        <h2>
-          What we do
-        </h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna
-        </p>
-      </div>
-      <div class="do_container">
-        <div class="box arrow-start arrow_bg">
-          <div class="img-box">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/d-1.png" alt="">
-          </div>
-          <div class="detail-box">
-            <h6>
-              Marketing
-            </h6>
-          </div>
-        </div>
-        <div class="box arrow-middle arrow_bg">
-          <div class="img-box">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/d-2.png" alt="">
-          </div>
-          <div class="detail-box">
-            <h6>
-              Development
-            </h6>
-          </div>
-        </div>
-        <div class="box arrow-middle arrow_bg">
-          <div class="img-box">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/d-3.png" alt="">
-          </div>
-          <div class="detail-box">
-            <h6>
-              Html5
-            </h6>
-          </div>
-        </div>
-        <div class="box arrow-end arrow_bg">
-          <div class="img-box">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/d-4.png" alt="">
-          </div>
-          <div class="detail-box">
-            <h6>
-              Css
-            </h6>
-          </div>
-        </div>
-        <div class="box ">
-          <div class="img-box">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/d-5.png" alt="">
-          </div>
-          <div class="detail-box">
-            <h6>
-              Wordpress
-            </h6>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <!-- end do section -->
-
-  <!-- who section -->
-
-  <section class="who_section ">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-5">
-          <div class="img-box">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/who-img.jpg" alt="">
-          </div>
-        </div>
-        <div class="col-md-7">
-          <div class="detail-box">
-            <div class="heading_container">
-              <h2>
-                WHO WE ARE?
-              </h2>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis nostrudLorem ipsum dolor sit amet, consectetur
-              adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud
-            </p>
-            <div>
-              <a href="">
-                Read More
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <!-- end who section -->
-
-
-  <!-- work section -->
-  <section class="work_section layout_padding">
-    <div class="container">
-      <div class="heading_container">
-        <h2>
-          CREATIVE WORKS
-        </h2>
-        <p>
-          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation
-        </p>
-      </div>
-      <div class="work_container layout_padding2">
-        <div class="box b-1">
-          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/w-1.png" alt="">
-        </div>
-        <div class="box b-2">
-          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/w-2.png" alt="">
-
-        </div>
-        <div class="box b-3">
-          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/w-3.png" alt="">
-
-        </div>
-        <div class="box b-4">
-          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/w-4.png" alt="">
-
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <!-- end work section -->
-
-  <!-- client section -->
-  <section class="client_section">
-    <div class="container">
-      <div class="heading_container">
-        <h2>
-          WHAT CUSTOMERS SAY
-        </h2>
-      </div>
-      <div class="carousel-wrap ">
-        <div class="owl-carousel">
-          <div class="item">
-            <div class="box">
-              <div class="img-box">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/c-1.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Tempor incididunt <br>
-                  <span>
-                    Dipiscing elit
-                  </span>
-                </h5>
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/quote.png" alt="">
-                <p>
-                  Dipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="box">
-              <div class="img-box">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/c-2.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Tempor incididunt <br>
-                  <span>
-                    Dipiscing elit
-                  </span>
-                </h5>
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/quote.png" alt="">
-                <p>
-                  Dipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="box">
-              <div class="img-box">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/c-3.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Tempor incididunt <br>
-                  <span>
-                    Dipiscing elit
-                  </span>
-                </h5>
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/quote.png" alt="">
-                <p>
-                  Dipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <!-- end client section -->
-
-  <!-- target section -->
-  <section class="target_section layout_padding2">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-3 col-sm-6">
-          <div class="detail-box">
-            <h2>
-              1000+
-            </h2>
-            <h5>
-              Years of Business
-            </h5>
-          </div>
-        </div>
-        <div class="col-md-3 col-sm-6">
-          <div class="detail-box">
-            <h2>
-              20000+
-            </h2>
-            <h5>
-              Projects Delivered
-            </h5>
-          </div>
-        </div>
-        <div class="col-md-3 col-sm-6">
-          <div class="detail-box">
-            <h2>
-              10000+
-            </h2>
-            <h5>
-              Satisfied Customers
-            </h5>
-          </div>
-        </div>
-        <div class="col-md-3 col-sm-6">
-          <div class="detail-box">
-            <h2>
-              1500+
-            </h2>
-            <h5>
-              Cups of Coffee
-            </h5>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <!-- end target section -->
-
-
-  <!-- contact section -->
-
-  <section class="contact_section layout_padding">
-    <div class="container">
-
-      <div class="heading_container">
-        <h2>
-          Request A Call Back
-        </h2>
-      </div>
-      <div class="">
-        <div class="">
-          <div class="row">
-            <div class="col-md-9 mx-auto">
-              <div class="contact-form">
-                <form action="">
-                  <div>
-                    <input type="text" placeholder="Full Name ">
+        <div class="row">
+          <?php
+              while(have_posts()) {
+              the_post();  
+              $imagepath = wp_get_attachment_image_src(get_post_thumbnail_id(),'large');  
+          ?>
+           
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top img-fluid" src="<?php echo $imagepath[0] ?>" alt="Card image cap" >
+                    <div class="card-body">
+                      <h5 class="card-title"><?php the_title() ?></h5>
+                      <?php the_date() ?>
+                      <p class="card-text"><?php the_excerpt() ?></p>
+                      <a href="#" class="btn btn-primary">See More</a>
+                    </div>
                   </div>
-                  <div>
-                    <input type="text" placeholder="Phone Number">
-                  </div>
-                  <div>
-                    <input type="email" placeholder="Email Address">
-                  </div>
-                  <div>
-                    <input type="text" placeholder="Message" class="input_message">
-                  </div>
-                  <div class="d-flex justify-content-center">
-                    <button type="submit" class="btn_on-hover">
-                      Send
-                    </button>
-                  </div>
-                </form>
-              </div>
             </div>
-          </div>
+          <?php
+              }
+          ?>  
         </div>
-      </div>
-      <div class="map_img-box">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/map-img.png" alt="">
-      </div>
     </div>
-  </section>
-
-
-  <!-- end contact section -->
+    <!-- Body Contend is ended here -->
 
 
 
 
-<?php 
-  get_footer();
-?>
+   <?php
+    get_footer();
+    ?>
